9d29ae41d0574eabae096001cf8cbab94cbf5693
Lakever <givster@yandex.ru>
2025-04-09 18:31:46 +0300

    Setup gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,25 @@
 plugins {
-    id("java")
+    java // Подключаем плагин для Java
+    application // Подключаем плагин application
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+    mavenCentral() // Используем Maven Central для зависимостей
 }
 
 dependencies {
+    // Используем BOM для JUnit Jupiter
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
-    useJUnitPlatform()
+    useJUnitPlatform() // Используем платформу JUnit 5
+}
+
+application {
+    mainClass.set("hexlet.code.App") // Указываем главный класс
 }
