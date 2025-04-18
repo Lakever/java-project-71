cb70a8bb8c3c2c0b68dae26efd53e4d46b78e843
Lakever <givster@yandex.ru>
2025-04-18 20:16:39 +0300

    Настройка окружения

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,44 @@
-
 plugins {
-    java // Подключаем плагин для Java
-    application // Подключаем плагин application
+    java
+    application
     checkstyle
+    jacoco
     id("org.sonarqube") version "6.1.0.5360"
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.6.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+// Генерация отчёта
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
+
+// Чтобы sonar запускался после jacoco
+tasks.named("sonar") {
+    dependsOn(tasks.jacocoTestReport)
+}
+
+// SonarCloud
 sonar {
     properties {
         property("sonar.projectKey", "Lakever_java-project-71")
@@ -16,35 +46,3 @@ sonar {
         property("sonar.host.url", "https://sonarcloud.io")
     }
 }
-
-repositories {
-    mavenCentral() // Используем Maven Central для зависимостей
-}
-
-dependencies {
-
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("info.picocli:picocli:4.6.3")
-    implementation ("com.fasterxml.jackson.core:jackson-databind:2.16.1")
-//    checkstyle("com.puppycrawl.tools:checkstyle:${checkstyle.toolVersion}")
-}
-
-
-tasks.test {
-    useJUnitPlatform() // Используем платформу JUnit 5
-}
-
-
-
-application {
-    mainClass.set("hexlet.code.App") // Указываем главный класс
-}
-//checkstyle {
-//    toolVersion = '10.12.1' // Актуальная версия на момент ответа
-//    configDirectory = file("${rootDir}/config/checkstyle") // Указываем директорию
-//
-//    // Настройки отчетов
-//    reportsDir = file("${buildDir}/reports/checkstyle")
-//    ignoreFailures = false // Фаил билд при ошибках
-//}
