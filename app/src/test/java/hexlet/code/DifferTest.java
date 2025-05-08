75fbc9fbfd3d182f6e273a806131c2cdaeeb2839
Lakever <givster@yandex.ru>
2025-05-08 16:33:08 +0300

    refactoringTest

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,4 +1,6 @@
 package hexlet.code;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static hexlet.code.Differ.generate;
@@ -22,55 +24,68 @@ public class DifferTest {
         var result = Files.readString(getFixturePath(fileName));
         return result.trim();
     }
-    @Test
-    public void testingGenerateJson() throws IOException {
-        System.out.println("START");
-//        var filePath = getFixturePath("DefferTest.txt"); // получили путь
-        var expected = readFixture("DifferTestJson.txt");
-        var filePath1 = getFixturePath("file1.json").toString();
-        var filePath2 = getFixturePath("file2.json").toString();
 
-        System.out.println(expected);
-        var style = "stylish";
-        var actual = generate(filePath1, filePath2, style);
-        assertEquals(expected, actual, "Something wrong LOOOOOOOOOOOL");
+    @BeforeEach
+    public void setUp() throws IOException {
+        var expectedPlain = readFixture("DifferTestPlain");
+        var expectedStylish = readFixture("DifferTestStylish");
+        var expectedJson = readFixture("DifferTestJson");
+
     }
 
     @Test
-    public void testingGenerateYaml() throws IOException {
-        System.out.println("START");
-//        var filePath = getFixturePath("DefferTest.txt"); // получили путь
-        var expected = readFixture("DifferTestYaml.txt");
-        var filePath1 = getFixturePath("file1Test.yaml").toString();
-        var filePath2 = getFixturePath("file2Test.yaml").toString();
-
-        System.out.println(expected);
-
-        var style = "stylish";
-        var actual = generate(filePath1, filePath2, style);
-        assertEquals(expected, actual, "Something wrong LOOOOOOOOOOOL");
-    }
-
-    @Test
-    public void testingGenerateYamltoPlain() throws IOException {
-        System.out.println("START");
-//        var filePath = getFixturePath("DefferTest.txt"); // получили путь
-        var expected = readFixture("DifferTestPlain.txt");
-        var filePath1 = getFixturePath("file1TestNestingYaml.yaml").toString();
-        var filePath2 = getFixturePath("file2TestNestingYaml.yaml").toString();
-
-        System.out.println(expected);
-
-        var style = "plain";
-        var actual = generate(filePath1, filePath2, style);
-        assertEquals(expected, actual, "Something wrong LOOOOOOOOOOOL");
+    public void generateTest() { // и здесь у нас будет 1 тест на всё приложение
 
     }
+//    private static String exeTest(String fixture, String fileName1, String fileName2, String style) throws IOException {
+//        var filePath1 = getFixturePath(fileName1).toString();
+//        var filePath2 = getFixturePath(fileName2).toString();
+//
+//        return generate(filePath1, filePath2, style);
+//    }
+
+//    @Test
+//    public void testingGenerateJson() throws IOException {
+//        var expected = readFixture("DifferTestJson.txt");
+//        var filePath1 = getFixturePath("file1.json").toString();
+//        var filePath2 = getFixturePath("file2.json").toString();
+//
+//        System.out.println(expected);
+//        var style = "stylish";
+//        var actual = generate(filePath1, filePath2, style);
+//        assertEquals(expected, actual, "Something wrong LOOOOOOOOOOOL");
+//    }
+
+//    @Test
+//    public void testingGenerateYaml() throws IOException {
+//        var expected = readFixture("DifferTestYaml.txt");
+//        var filePath1 = getFixturePath("file1Test.yaml").toString();
+//        var filePath2 = getFixturePath("file2Test.yaml").toString();
+//
+//        System.out.println(expected);
+//
+//        var style = "stylish";
+//        var actual = generate(filePath1, filePath2, style);
+//        assertEquals(expected, actual, "Something wrong LOOOOOOOOOOOL");
+//    }
+
+//    @Test
+//    public void testingGenerateYamltoPlain() throws IOException {
+//        System.out.println("START");
+//        var expected = readFixture("DifferTestPlain.txt");//
+//        var filePath1 = getFixturePath("file1TestNesting.yaml").toString();
+//        var filePath2 = getFixturePath("file2TestNesting.yaml").toString();
+//
+//        System.out.println(expected);
+//
+//        var style = "plain";
+//        var actual = generate(filePath1, filePath2, style);
+//        assertEquals(expected, actual, "Something wrong LOOOOOOOOOOOL");
+//
+//    }
 
     @Test
     public void testingGenerateJsontoPlain() throws IOException {
-        System.out.println("START");
-//        var filePath = getFixturePath("DefferTest.txt"); // получили путь
         var expected = readFixture("DifferTestPlain.txt");
         var filePath1 = getFixturePath("file1TestNesting.json").toString();
         var filePath2 = getFixturePath("file2TestNesting.json").toString();
@@ -79,7 +94,7 @@ public class DifferTest {
 
         var style = "plain";
         var actual = generate(filePath1, filePath2, style);
-        assertEquals(expected, actual, "Something wrong LOOOOOOOOOOOL");
+        assertEquals(expected, actual, "Something wrong");
 
     }
 
@@ -104,8 +119,8 @@ public class DifferTest {
         System.out.println("START");
 //        var filePath = getFixturePath("DefferTest.txt"); // получили путь
         var expected = readFixture("DifferTestJsonToJson.txt");
-        var filePath1 = getFixturePath("file1TestNestingYaml.yaml").toString();
-        var filePath2 = getFixturePath("file2TestNestingYaml.yaml").toString();
+        var filePath1 = getFixturePath("file1TestNesting.yaml").toString();
+        var filePath2 = getFixturePath("file2TestNesting.yaml").toString();
 
         System.out.println(expected);
 
--- app/src/test/java/hexlet/code/ParserTest.java
+++ app/src/test/java/hexlet/code/ParserTest.java
@@ -12,6 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static hexlet.code.Differ.generate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -23,18 +24,81 @@ public class ParserTest {
                 .normalize();
     }
 
+    private static String readFixture(String fileName) throws IOException {
+        var result = Files.readString(getFixturePath(fileName));
+        return result.trim();
+    }
+
+    @Test
+    public void testGenerate() throws IOException {
+        var file1 = getFixturePath("file1TestNesting.json").toString();
+        var file2 = getFixturePath("file2TestNesting.json").toString();
+
+
+        var expected = readFixture("DifferTestStylish.txt");
+        var actual = generate(file1, file2, "stylish");
+
+        assertEquals(expected, actual);
+    }
+
 //    @Test
-//    public void testParseValidJson() throws IOException {
-//        String testFileName = "file2Test.json";
-//        Path testFilePath = getFixturePath(testFileName);
+//    public void testGenerate() throws IOException {
+//        String testFileName = "file2TestNesting.json";
+//        Path testFilePath = getFixturePath(testFileName); // вот эта штука берет путь до файла
 //
-//        assertTrue(Files.exists(testFilePath));
+//        assertTrue(Files.exists(testFilePath), "File not found");
 //
-//        Map<String, Object> expected = Map.of(
-//                "host", "hexlet.io",
-//                "timeout", 20,
-//                "verbose", true
-//        );
+//        Map<String, Object> expected = new HashMap<>();
+//        expected.put("setting1", "Another value");
+//        expected.put("setting2", 300);
+//        expected.put("setting3", "none");
+//        expected.put("key2", "value2");
+//        expected.put("numbers1", List.of(1, 2, 3, 4));
+//        expected.put("numbers2", List.of(22, 33, 44, 55));
+//        expected.put("id", null);
+//        expected.put("default", List.of("value1", "value2"));
+//        expected.put("checked", true);
+//        expected.put("numbers4", List.of(4, 5, 6));
+//        expected.put("chars1", List.of("a", "b", "c"));
+//        expected.put("chars2", false);
+//        //а тут добавлю вложенный объект
+//        Map<String, Object> obj1 = new HashMap<>();
+//        obj1.put("nestedKey", "value");
+//        obj1.put("isNested", true);
+//
+//        expected.put("obj1", obj1);
+//
+//        Map<String, Object> actual = PathFile.fromFile(testFilePath.toString());
+//
+//        assertEquals(expected, actual);
+//    }
+
+//    @Test
+//    public void testParseNestingJson() throws IOException {
+//        String testFileName = "file2TestNesting.json";
+//        Path testFilePath = getFixturePath(testFileName); // вот эта штука берет путь до файла
+//
+//        assertTrue(Files.exists(testFilePath), "File not found");
+//
+//        Map<String, Object> expected = new HashMap<>();
+//        expected.put("setting1", "Another value");
+//        expected.put("setting2", 300);
+//        expected.put("setting3", "none");
+//        expected.put("key2", "value2");
+//        expected.put("numbers1", List.of(1, 2, 3, 4));
+//        expected.put("numbers2", List.of(22, 33, 44, 55));
+//        expected.put("id", null);
+//        expected.put("default", List.of("value1", "value2"));
+//        expected.put("checked", true);
+//        expected.put("numbers4", List.of(4, 5, 6));
+//        expected.put("chars1", List.of("a", "b", "c"));
+//        expected.put("chars2", false);
+//        //а тут добавлю вложенный объект
+//        Map<String, Object> obj1 = new HashMap<>();
+//        obj1.put("nestedKey", "value");
+//        obj1.put("isNested", true);
+//
+//        expected.put("obj1", obj1);
 //
 //        Map<String, Object> actual = PathFile.fromFile(testFilePath.toString());
 //
@@ -42,84 +106,34 @@ public class ParserTest {
 //    }
 //
 //    @Test
-//    public void testParseValidYaml() throws IOException {
-//        String testFileName = "file2Test.yaml";
-//        Path testFilePath = getFixturePath(testFileName);
+//    public void testParseNestingYaml() throws IOException {
+//        String testFileName = "file2TestNesting.yaml";
+//        Path testFilePath = getFixturePath(testFileName); // вот эта штука берет путь до файла
 //
 //        assertTrue(Files.exists(testFilePath));
 //
-//        Map<String, Object> expected = Map.of(
-//                "host", "hexlet.io",
-//                "timeout", 20,
-//                "verbose", true
-//        );
+//        Map<String, Object> expected = new HashMap<>();
+//        expected.put("setting1", "Another value");
+//        expected.put("setting2", 300);
+//        expected.put("setting3", "none");
+//        expected.put("key2", "value2");
+//        expected.put("numbers1", List.of(1, 2, 3, 4));
+//        expected.put("numbers2", List.of(22, 33, 44, 55));
+//        expected.put("id", null);
+//        expected.put("default", List.of("value1", "value2"));
+//        expected.put("checked", true);
+//        expected.put("numbers4", List.of(4, 5, 6));
+//        expected.put("chars1", List.of("a", "b", "c"));
+//        expected.put("chars2", false);
+//        //а тут добавлю вложенный объект
+//        Map<String, Object> obj1 = new HashMap<>();
+//        obj1.put("nestedKey", "value");
+//        obj1.put("isNested", true);
+//        // и положу его к остальным
+//        expected.put("obj1", obj1);
 //
 //        Map<String, Object> actual = PathFile.fromFile(testFilePath.toString());
 //
 //        assertEquals(expected, actual);
 //    }
-
-    @Test
-    public void testParseNestingJson() throws IOException {
-        String testFileName = "file2TestNesting.json";
-        Path testFilePath = getFixturePath(testFileName); // вот эта штука берет путь до файла
-
-        assertTrue(Files.exists(testFilePath), "File not found");
-
-        Map<String, Object> expected = new HashMap<>();
-        expected.put("setting1", "Another value");
-        expected.put("setting2", 300);
-        expected.put("setting3", "none");
-        expected.put("key2", "value2");
-        expected.put("numbers1", List.of(1, 2, 3, 4));
-        expected.put("numbers2", List.of(22, 33, 44, 55));
-        expected.put("id", null);
-        expected.put("default", List.of("value1", "value2"));
-        expected.put("checked", true);
-        expected.put("numbers4", List.of(4, 5, 6));
-        expected.put("chars1", List.of("a", "b", "c"));
-        expected.put("chars2", false);
-        //а тут добавлю вложенный объект
-        Map<String, Object> obj1 = new HashMap<>();
-        obj1.put("nestedKey", "value");
-        obj1.put("isNested", true);
-
-        expected.put("obj1", obj1);
-
-        Map<String, Object> actual = PathFile.fromFile(testFilePath.toString());
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testParseNestingYaml() throws IOException {
-        String testFileName = "file2TestNestingYaml.yaml";
-        Path testFilePath = getFixturePath(testFileName); // вот эта штука берет путь до файла
-
-        assertTrue(Files.exists(testFilePath));
-
-        Map<String, Object> expected = new HashMap<>();
-        expected.put("setting1", "Another value");
-        expected.put("setting2", 300);
-        expected.put("setting3", "none");
-        expected.put("key2", "value2");
-        expected.put("numbers1", List.of(1, 2, 3, 4));
-        expected.put("numbers2", List.of(22, 33, 44, 55));
-        expected.put("id", null);
-        expected.put("default", List.of("value1", "value2"));
-        expected.put("checked", true);
-        expected.put("numbers4", List.of(4, 5, 6));
-        expected.put("chars1", List.of("a", "b", "c"));
-        expected.put("chars2", false);
-        //а тут добавлю вложенный объект
-        Map<String, Object> obj1 = new HashMap<>();
-        obj1.put("nestedKey", "value");
-        obj1.put("isNested", true);
-        // и положу его к остальным
-        expected.put("obj1", obj1);
-
-        Map<String, Object> actual = PathFile.fromFile(testFilePath.toString());
-
-        assertEquals(expected, actual);
-    }
 }
